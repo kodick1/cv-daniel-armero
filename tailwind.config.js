0e74178840802af8a1242fc0f408fd78103898d9
Daniel Armero <135051283+kodick1@users.noreply.github.com>
2025-03-24 19:04:04 -0500

    cv-daniel
    upload cv, responsive and tailwind with react

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Asegúrate de incluir las rutas correctas
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
